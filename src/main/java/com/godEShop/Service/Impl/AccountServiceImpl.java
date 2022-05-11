2afff8217e3ed36bbef79dee4f07fd3e2b83a0ec
nghiatran1304 <nghiatt130497@gmail.com>
2022-05-11 10:36:53 +0700

    Update Service and ServiceImp

---
+++ src/main/java/com/godEShop/Service/Impl/AccountServiceImpl.java
@@ -0,0 +1,10 @@
+package com.godEShop.Service.Impl;
+
+import org.springframework.stereotype.Service;
+
+import com.godEShop.Service.AccountService;
+
+@Service
+public class AccountServiceImpl implements AccountService{
+
+}
