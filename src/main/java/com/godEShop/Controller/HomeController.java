a7a20f4f09ea4628b6e63fbb00e66a1b111690d6
nghiatran1304 <nghiatt130497@gmail.com>
2022-05-13 17:53:11 +0700

    Update

--- src/main/java/com/godEShop/Controller/HomeController.java
+++ src/main/java/com/godEShop/Controller/HomeController.java
@@ -1,16 +1,13 @@
 package com.godEShop.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-   
-  
+
     @GetMapping("/index")
-    public String index(Model model) {
-//	model.addAttribute("isHomePage", true);
+    public String index() {
 	return "layout/homepage";
     }
 
@@ -23,44 +20,40 @@ public class HomeController {
     public String loginPage() {
 	return "account/login";
     }
-    
+
     @GetMapping("/register")
     public String registerPage() {
 	return "account/register";
     }
-    
+
     @GetMapping("/information")
     public String informationPage() {
 	return "account/information";
     }
-    
+
     @GetMapping("/forgotpassword")
     public String forgotpasswordPage() {
 	return "account/forgot-password";
     }
-    
+
     @GetMapping("/viewcart")
     public String viewCartPage() {
 	return "cart/view-cart";
     }
-    
+
     @GetMapping("/checkout")
     public String checkoutPage() {
 	return "order/checkout";
     }
-    
-    
+
     @GetMapping("/singleproduct")
     public String singleproductPage() {
 	return "product/single-product";
     }
-    
-    
+
     @GetMapping("/product")
-    public String productPage(Model model) {
-//	model.addAttribute("isHomePage", false);
+    public String productPage() {
 	return "product/product";
     }
-    
-    
+
 }
