4309996989dfffaa929cc9b6ea10d091164de75c
nghiatran1304 <nghiatt130497@gmail.com>
2022-06-12 18:50:03 +0700

    Update fix discount

--- src/main/java/com/godEShop/Dao/ProductDAO.java
+++ src/main/java/com/godEShop/Dao/ProductDAO.java
@@ -39,8 +39,10 @@ public interface ProductDAO extends JpaRepository<Product, Long> {
 	    + "FULL JOIN p.brand pb " 
 	    + "FULL JOIN p.productDiscounts pd " 
 	    + "FULL JOIN p.category c "
-	    + "WHERE p.isDeleted = 0 AND c.available = 0 AND (p.name LIKE ?1 OR c.name LIKE ?1) AND c.name LIKE ?2 AND pb.name LIKE ?3 "
-	    + "GROUP BY p.id, c.id, p.name, p.price, p.createDate, c.name, pd.discount, p.detail, p.quantity ")
+	    + "WHERE p.isDeleted = 0 AND c.available = 0 "
+	    + "AND pd.id NOT IN (SELECT pd.id FROM ProductDiscount AS pd FULL JOIN pd.product p WHERE pd.endDate < GETDATE() GROUP BY pd.id) "
+	    + "AND (p.name LIKE ?1 OR c.name LIKE ?1) AND c.name LIKE ?2 AND pb.name LIKE ?3 "
+	    + "GROUP BY p.id, c.id, p.name, p.price, p.createDate, c.name, pd.discount, p.detail, p.quantity, pd.createDate")
 	   //+ "HAVING MAX(pd.endDate) >= GETDATE() OR MAX(pd.endDate) IS NULL ")
     Page<ProductShopDto> productShop(String kws, String categoryName, String brandName, Pageable pageable);
 
--- src/main/java/com/godEShop/Dto/ProductShopDto.java
+++ src/main/java/com/godEShop/Dto/ProductShopDto.java
@@ -5,19 +5,13 @@ import java.util.Date;
 
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @ToString
-public class ProductShopDto implements Serializable{
-    
+public class ProductShopDto implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     private Long productId;
     private Integer productCategoryId;
@@ -32,14 +26,37 @@ public class ProductShopDto implements Serializable{
     private Date productEndDiscount;
     private Integer productQuantity;
     private Date productStartDiscount;
-    
+
+    public ProductShopDto(Long productId, Integer productCategoryId, String productName, Double productPrice,
+	    Date productCreateDate, String productCategoryName, String productFirstImage, Integer productEvaluation,
+	    Integer productDiscount, String productDetail, Date productEndDiscount, Integer productQuantity,
+	    Date productStartDiscount) {
+	this.productId = productId;
+	this.productCategoryId = productCategoryId;
+	this.productName = productName;
+	this.productPrice = productPrice;
+	this.productCreateDate = productCreateDate;
+	this.productCategoryName = productCategoryName;
+	this.productFirstImage = productFirstImage;
+	this.productEvaluation = productEvaluation;
+	this.productDiscount = productDiscount;
+	this.productDetail = productDetail;
+	this.productEndDiscount = productEndDiscount;
+	this.productQuantity = productQuantity;
+	this.productStartDiscount = productStartDiscount;
+    }
+
+    public ProductShopDto() {
+
+    }
+
     public double getFinalPrice() {
 	Date nowTime = new Date();
-	
-	if(this.productEndDiscount == null || this.productStartDiscount.compareTo(nowTime) == 1) {
+
+	if (this.productEndDiscount == null || this.productStartDiscount.compareTo(nowTime) == 1) {
 	    return this.productPrice;
 	}
-	if(this.productEndDiscount.compareTo(nowTime) == -1 ) {
+	if (this.productEndDiscount.compareTo(nowTime) == -1) {
 	    return this.productPrice;
 	}
 	double p = this.productPrice;
@@ -47,5 +64,109 @@ public class ProductShopDto implements Serializable{
 	double rs = p - (p * d / 100.0);
 	return rs;
     }
-    
+
+    public Long getProductId() {
+	return productId;
+    }
+
+    public void setProductId(Long productId) {
+	this.productId = productId;
+    }
+
+    public Integer getProductCategoryId() {
+	return productCategoryId;
+    }
+
+    public void setProductCategoryId(Integer productCategoryId) {
+	this.productCategoryId = productCategoryId;
+    }
+
+    public String getProductName() {
+	return productName;
+    }
+
+    public void setProductName(String productName) {
+	this.productName = productName;
+    }
+
+    public Double getProductPrice() {
+	return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+	this.productPrice = productPrice;
+    }
+
+    public Date getProductCreateDate() {
+	return productCreateDate;
+    }
+
+    public void setProductCreateDate(Date productCreateDate) {
+	this.productCreateDate = productCreateDate;
+    }
+
+    public String getProductCategoryName() {
+	return productCategoryName;
+    }
+
+    public void setProductCategoryName(String productCategoryName) {
+	this.productCategoryName = productCategoryName;
+    }
+
+    public String getProductFirstImage() {
+	return productFirstImage;
+    }
+
+    public void setProductFirstImage(String productFirstImage) {
+	this.productFirstImage = productFirstImage;
+    }
+
+    public Integer getProductEvaluation() {
+	return productEvaluation;
+    }
+
+    public void setProductEvaluation(Integer productEvaluation) {
+	this.productEvaluation = productEvaluation;
+    }
+
+    public Integer getProductDiscount() {
+	return productDiscount;
+    }
+
+    public void setProductDiscount(Integer productDiscount) {
+	this.productDiscount = productDiscount;
+    }
+
+    public String getProductDetail() {
+	return productDetail;
+    }
+
+    public void setProductDetail(String productDetail) {
+	this.productDetail = productDetail;
+    }
+
+    public Date getProductEndDiscount() {
+	return productEndDiscount;
+    }
+
+    public void setProductEndDiscount(Date productEndDiscount) {
+	this.productEndDiscount = productEndDiscount;
+    }
+
+    public Integer getProductQuantity() {
+	return productQuantity;
+    }
+
+    public void setProductQuantity(Integer productQuantity) {
+	this.productQuantity = productQuantity;
+    }
+
+    public Date getProductStartDiscount() {
+	return productStartDiscount;
+    }
+
+    public void setProductStartDiscount(Date productStartDiscount) {
+	this.productStartDiscount = productStartDiscount;
+    }
+
 }
