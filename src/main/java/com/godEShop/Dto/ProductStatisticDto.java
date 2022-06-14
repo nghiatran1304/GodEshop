8790a154288169ffc8b9e743f9bf1cfc7b654208
hainguyentran14 <hainguyentran14@gmail.com>
2022-06-14 09:53:11 +0700

    Update fix Statistic

---
+++ src/main/java/com/godEShop/Dto/ProductStatisticDto.java
@@ -0,0 +1,25 @@
+package com.godEShop.Dto;
+
+import java.io.Serializable;
+
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductStatisticDto implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long productId;
+    private String productName;
+    private int quantityleft;
+    private Long quantity;
+}
---
+++ src/main/java/com/godEShop/RestController/StatisticRestController.java
@@ -0,0 +1,24 @@
+package com.godEShop.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.godEShop.Dao.ProductDAO;
+import com.godEShop.Dto.ProductStatisticDto;
+
+@CrossOrigin("*")
+@RestController
+public class StatisticRestController {
+	@Autowired
+	ProductDAO prodDAO;
+	
+	@GetMapping("/rest/statistic/products")
+	public List<ProductStatisticDto> getProductsStatistic() {
+		return prodDAO.getProductStatistic();
+	}
+}
