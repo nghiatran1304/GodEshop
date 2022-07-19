4a980bf024caf26f4e93272debf0dff52ac3a7e9
nghiatran1304 <nghiatt130497@gmail.com>
2022-07-19 19:32:13 +0700

    Update Accessory

--- src/main/java/com/godEShop/RestController/AccessoryRestController.java
+++ src/main/java/com/godEShop/RestController/AccessoryRestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.godEShop.Dto.ProductAccessoryInfoDto;
-import com.godEShop.Dto.ProductWatchInfoDto;
 import com.godEShop.Entity.Accessory;
 import com.godEShop.Entity.BraceletMaterial;
 import com.godEShop.Entity.Brand;
@@ -29,66 +28,67 @@ import com.godEShop.Service.ProductService;
 @CrossOrigin("*")
 @RestController
 public class AccessoryRestController {
-	@Autowired
-	AccessoryService accessoryService;
-	@Autowired
-	CategoryService categoryService;
-	@Autowired
-	BrandService brandService;
-	@Autowired
-	ProductService productService;
-	@Autowired
-	BraceletMaterialService bmService;
-	@Autowired
-	ProductPhotoService productPhotoService;
+    @Autowired
+    AccessoryService accessoryService;
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    BrandService brandService;
+    @Autowired
+    ProductService productService;
+    @Autowired
+    BraceletMaterialService bmService;
+    @Autowired
+    ProductPhotoService productPhotoService;
 
-	public static Product productInserted1 = new Product();
+    public static Product productInserted1 = new Product();
 
-	@GetMapping("/rest/accessories")
-	public List<ProductAccessoryInfoDto> getAll() {
-		return accessoryService.lstFullInfoAccessory();
-	}
+    @GetMapping("/rest/accessories")
+    public List<ProductAccessoryInfoDto> getAll() {
+	return accessoryService.lstFullInfoAccessory();
+    }
 
-	@PostMapping("/rest/post-product")
-	public Product create(@RequestBody ProductAccessoryInfoDto product) {
-		System.out.println(product == null ? " >>>> NULL PRODUCT" : " >>>>> NOT NULL PRODUCT");
-		System.out.println(product.getCategoryId() == null ? " >>>> NULL CATEGORY ID "
-				: " >>>>> NOT NULL CATEGORY ID : " + product.getCategoryId());
-		System.out.println(product.getBrandId() == null ? " >>>> NULL BRAND ID"
-				: " >>>>> NOT NULL BRAND ID: " + product.getBrandId());
+    @PostMapping("/rest/post-product")
+    public Product create(@RequestBody ProductAccessoryInfoDto product) {
+	System.out.println(product == null ? " >>>> NULL PRODUCT" : " >>>>> NOT NULL PRODUCT");
+	System.out.println(product.getCategoryId() == null ? " >>>> NULL CATEGORY ID "
+		: " >>>>> NOT NULL CATEGORY ID : " + product.getCategoryId());
+	System.out.println(product.getBrandId() == null ? " >>>> NULL BRAND ID"
+		: " >>>>> NOT NULL BRAND ID: " + product.getBrandId());
 
-		Category c = categoryService.getById(13);
-		Brand b = brandService.getById(product.getBrandId());
-		Product p = new Product();
-		p.setCreateDate(product.getProductCreateDate());
-		p.setDetail(product.getProductDetail());
-		p.setMadeIn(product.getProductMadeIn());
-		p.setName(product.getProductName());
-		p.setIsDeleted(product.getProductIsDeteled());
-		p.setPrice(product.getProductPrice());
-		p.setQuantity(product.getProductQuantity());
-		p.setWarranty(product.getProductWarranty());
-		p.setBrand(b);
-		p.setCategory(c);
+	Category c = categoryService.getById(13);
+	Brand b = brandService.getById(product.getBrandId());
+	Product p = new Product();
+	p.setCreateDate(product.getProductCreateDate());
+	p.setDetail(product.getProductDetail());
+	p.setMadeIn(product.getProductMadeIn());
+	p.setName(product.getProductName());
+	p.setIsDeleted(product.getProductIsDeteled());
+	p.setPrice(product.getProductPrice());
+	p.setQuantity(product.getProductQuantity());
+	p.setWarranty(product.getProductWarranty());
+	p.setBrand(b);
+	p.setCategory(c);
 
-		productInserted1 = p;
+	productInserted1 = p;
 
-		return productService.create(p);
-	}
-	
-	@PostMapping("/rest/post-accessory")
-	public Accessory createAccessory(@RequestBody ProductAccessoryInfoDto product) {
-		System.out.println(
-				productInserted1 == null ? " >>> NULL PRODUCT RESTCONTROLLER" : " >>> NOT NULL PRODUCT RESTCONTROLLER");
-		System.out
-				.println(product == null ? " >>> NULL PRODUCT RESTCONTROLLER" : " >>> NOT NULL PRODUCT RESTCONTROLLER");
-		BraceletMaterial bm = bmService.getById(product.getBraceletMaterialId());
-		Accessory ac = new Accessory();
-		ac.setColors(product.getAccessoryColor());
-		ac.setBraceletMaterial(bm);
-		ac.setProduct(productInserted1);
-		return accessoryService.create(ac);
-	}
+	return productService.create(p);
+    }
+
+    @PostMapping("/rest/post-accessory")
+    public Accessory createAccessory(@RequestBody ProductAccessoryInfoDto product) {
+	System.out.println(
+		productInserted1 == null ? " >>> NULL PRODUCT RESTCONTROLLER" : " >>> NOT NULL PRODUCT RESTCONTROLLER");
+	System.out
+		.println(product == null ? " >>> NULL PRODUCT RESTCONTROLLER" : " >>> NOT NULL PRODUCT RESTCONTROLLER");
+	BraceletMaterial bm = bmService.getById(product.getBraceletMaterialId());
+	Accessory ac = new Accessory();
+	ac.setColors(product.getAccessoryColor());
+	ac.setBraceletMaterial(bm);
+	ac.setProduct(productInserted1);
+	return accessoryService.create(ac);
+    }
+
     @PutMapping("/rest/put-product/{id}")
     public Product update(@PathVariable("id") Long id, @RequestBody ProductAccessoryInfoDto product) {
 	Brand b = brandService.getById(product.getBrandId());
@@ -108,40 +108,42 @@ public class AccessoryRestController {
 
 	return productService.update(p);
     }
-	@PutMapping("/rest/accessory/{id}")
-	public Accessory update(@PathVariable("id") Integer id, @RequestBody ProductAccessoryInfoDto product) {
-		BraceletMaterial bm = bmService.getById(product.getBraceletMaterialId());
-		Accessory ac = accessoryService.getById(product.getAccessoryId());
-		ac.setBraceletMaterial(bm);
-		ac.setColors(product.getAccessoryColor());
-		ac.setProduct(ac.getProduct());
-		return accessoryService.update(ac);
-	}
 
-	// them moi san pham
-	@PostMapping("/rest/post-photo")
-	public ProductPhoto update1(@RequestBody ProductAccessoryInfoDto product) {
-		ProductPhoto pp = new ProductPhoto();
-		pp.setId(product.getImageId());
-		pp.setProduct(ProductRestController.productInserted);
-		return productPhotoService.create(pp);
-	}
+    @PutMapping("/rest/accessory/{id}")
+    public Accessory update(@PathVariable("id") Integer id, @RequestBody ProductAccessoryInfoDto product) {
+	BraceletMaterial bm = bmService.getById(product.getBraceletMaterialId());
+	Accessory ac = accessoryService.getById(product.getAccessoryId());
+	ac.setBraceletMaterial(bm);
+	ac.setColors(product.getAccessoryColor());
+	ac.setProduct(ac.getProduct());
+	return accessoryService.update(ac);
+    }
 
-	// up them hinh
-	@PostMapping("/rest/post-photo-plus")
-	public ProductPhoto update(@RequestBody ProductAccessoryInfoDto product) {
-		ProductPhoto pp = new ProductPhoto();
-		pp.setId(product.getImageId());
-		pp.setProduct(productService.getById(product.getProductId()));
-		return productPhotoService.create(pp);
-	}
-	
-	 @GetMapping("/rest/products-accessory/search/{name}")
-	    public List<ProductAccessoryInfoDto> findBySearch(@PathVariable("name") String name) {
-		return accessoryService.lstSearchFullInfoAccessory("%" + name + "%");
-	    }
-	 @GetMapping("/rest/outOfSoonAccessory")
-	    public List<ProductAccessoryInfoDto> getAllOutOfSoon(){
-		return accessoryService.lstFullInfoAccessoryOutOfSoon();
-	    }
+    // them moi san pham
+    @PostMapping("/rest/post-photo")
+    public ProductPhoto update1(@RequestBody ProductAccessoryInfoDto product) {
+	ProductPhoto pp = new ProductPhoto();
+	pp.setId(product.getImageId());
+	pp.setProduct(ProductRestController.productInserted);
+	return productPhotoService.create(pp);
+    }
+
+    // up them hinh
+    @PostMapping("/rest/post-photo-plus")
+    public ProductPhoto update(@RequestBody ProductAccessoryInfoDto product) {
+	ProductPhoto pp = new ProductPhoto();
+	pp.setId(product.getImageId());
+	pp.setProduct(productService.getById(product.getProductId()));
+	return productPhotoService.create(pp);
+    }
+
+    @GetMapping("/rest/products-accessory/search/{name}")
+    public List<ProductAccessoryInfoDto> findBySearch(@PathVariable("name") String name) {
+	return accessoryService.lstSearchFullInfoAccessory("%" + name + "%");
+    }
+
+    @GetMapping("/rest/outOfSoonAccessory")
+    public List<ProductAccessoryInfoDto> getAllOutOfSoon() {
+	return accessoryService.lstFullInfoAccessoryOutOfSoon();
+    }
 }
--- src/main/java/com/godEShop/Service/AccessoryService.java
+++ src/main/java/com/godEShop/Service/AccessoryService.java
@@ -3,20 +3,19 @@ package com.godEShop.Service;
 import java.util.List;
 
 import com.godEShop.Dto.ProductAccessoryInfoDto;
-import com.godEShop.Dto.ProductWatchInfoDto;
 import com.godEShop.Entity.Accessory;
 
 public interface AccessoryService {
-	 List<ProductAccessoryInfoDto> lstFullInfoAccessory();
+    List<ProductAccessoryInfoDto> lstFullInfoAccessory();
 
-	Accessory create(Accessory ac);
+    Accessory create(Accessory ac);
 
-	Accessory getById(Integer accessoryId);
+    Accessory getById(Integer accessoryId);
 
-	Accessory update(Accessory ac);
+    Accessory update(Accessory ac);
 
-	List<ProductAccessoryInfoDto> lstSearchFullInfoAccessory(String string);
-	
-	List<ProductAccessoryInfoDto> lstFullInfoAccessoryOutOfSoon();
-	
+    List<ProductAccessoryInfoDto> lstSearchFullInfoAccessory(String string);
+
+    List<ProductAccessoryInfoDto> lstFullInfoAccessoryOutOfSoon();
+
 }
