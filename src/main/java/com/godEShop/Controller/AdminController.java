725278e1d5467899f074e2fb486368acdf938517
hainguyentran14 <hainguyentran14@gmail.com>
2022-07-23 17:54:23 +0700

    Update decor export file (excel)

--- src/main/java/com/godEShop/Controller/AdminController.java
+++ src/main/java/com/godEShop/Controller/AdminController.java
@@ -29,6 +29,9 @@ public class AdminController {
 	
 	@Autowired
 	UserDAO userDAO;
+	private String title = "";
+	private String keyword = "";
+	private String date = "";
 	
 	@GetMapping("/export/products")
 	public void exportProducts(HttpServletResponse response) throws IOException{
@@ -39,11 +42,14 @@ public class AdminController {
 		String headerKey = "Content-Disposition";
 		String headerValue = "attachment; filename=ProductsReport_" + currentDateTime + ".xlsx";
 		response.setHeader(headerKey, headerValue);
-			         
+			        
+		title = "Products Report".toUpperCase();
+		keyword = "All products";
+		date = "All time";
 		List<ProductsStatisticDto> listProducts = prodDAO.getProductStatistic();
 		List<ProductImageDto> listProductImage = prodDAO.getProductImage();
 			         
-		ProductsExcelWriter excelExporter = new ProductsExcelWriter(listProducts, listProductImage);
+		ProductsExcelWriter excelExporter = new ProductsExcelWriter(title, keyword, date, listProducts, listProductImage);
 			         
 		excelExporter.export(response);  
 	}
@@ -63,7 +69,54 @@ public class AdminController {
 		List<ProductsStatisticDto> listProducts = prodDAO.getProductStatisticByTime(newStart, newEnd);
 		List<ProductImageDto> listProductImage = prodDAO.getProductImage();
 			         
-		ProductsExcelWriter excelExporter = new ProductsExcelWriter(listProducts, listProductImage);
+		title = "Products Report".toUpperCase();
+		keyword = "All products";
+		date = "From " + start + " to " + end;
+		ProductsExcelWriter excelExporter = new ProductsExcelWriter(title, keyword, date, listProducts, listProductImage);
+			         
+		excelExporter.export(response);  
+	}
+	
+	@GetMapping("/export/products/name={name}")
+	public void exportProductsByName(HttpServletResponse response, @PathVariable("name") String name) throws IOException{
+		response.setContentType("application/octet-stream");
+	    DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+	    String currentDateTime = dateFormatter.format(new Date());
+	         
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=ProductsReport_" + currentDateTime + ".xlsx";
+		response.setHeader(headerKey, headerValue);
+			         
+		List<ProductsStatisticDto> listProducts = prodDAO.get1ProductStatistic("%" + name + "%");
+		List<ProductImageDto> listProductImage = prodDAO.getProductImage();
+		
+		title = "Products Report".toUpperCase();
+		keyword = "Filter by " + name;
+		date = "All time";
+		ProductsExcelWriter excelExporter = new ProductsExcelWriter(title, keyword, date, listProducts, listProductImage);
+			         
+		excelExporter.export(response);  
+	}
+	
+	@GetMapping("/export/products/name={name}/start={start}&end={end}")
+	public void exportProductsByName(HttpServletResponse response, @PathVariable("name") String name, @PathVariable("start") String start, @PathVariable("end") String end) throws IOException, ParseException{
+		response.setContentType("application/octet-stream");
+	    DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+	    String currentDateTime = dateFormatter.format(new Date());
+	         
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=ProductsReport_" + currentDateTime + ".xlsx";
+		response.setHeader(headerKey, headerValue);
+			         
+		Date newStart = new SimpleDateFormat("yyyy-MM-dd").parse(start);
+		Date newEnd = new SimpleDateFormat("yyyy-MM-dd").parse(end);
+		title = "Products Report".toUpperCase();
+		keyword = "Filter by " + name;
+		date = "From " + start + " to " + end;
+		List<ProductsStatisticDto> listProducts = prodDAO.get1ProductStatisticByTime("%" + name + "%", newStart, newEnd);
+		List<ProductImageDto> listProductImage = prodDAO.getProductImage();
+			         
+		ProductsExcelWriter excelExporter = new ProductsExcelWriter(title, keyword, date, listProducts, listProductImage);
 			         
 		excelExporter.export(response);  
 	}
@@ -80,13 +133,80 @@ public class AdminController {
          
         List<UsersStatisticDto> listUsers = userDAO.getAllUserStat();
          
-        UsersExcelWriter excelExporter = new UsersExcelWriter(listUsers);
+        title = "Users Report".toUpperCase();
+		keyword = "All users";
+		date = "All time";
+        UsersExcelWriter excelExporter = new UsersExcelWriter(title, keyword, date, listUsers);
+         
+        excelExporter.export(response);  
+	}
+	
+	@GetMapping("/export/users/start={start}&end={end}")
+	public void exportUsers(HttpServletResponse response, @PathVariable("start") String start, @PathVariable("end") String end) throws IOException,ParseException {
+		response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+         
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=UsersReport_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+        
+        Date newStart = new SimpleDateFormat("yyyy-MM-dd").parse(start);
+		Date newEnd = new SimpleDateFormat("yyyy-MM-dd").parse(end);
+        List<UsersStatisticDto> listUsers = userDAO.getAllUserStatByTime(newStart, newEnd);
+         
+        title = "Users Report".toUpperCase();
+		keyword = "All users";
+		date = "From " + start + " to " + end;
+        UsersExcelWriter excelExporter = new UsersExcelWriter(title, keyword, date, listUsers);
+         
+        excelExporter.export(response);  
+	}
+	
+	@GetMapping("/export/users/name={name}")
+	public void exportUsersByName(HttpServletResponse response, @PathVariable("name") String name) throws IOException {
+		response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+         
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=UsersReport_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+        
+        List<UsersStatisticDto> listUsers = userDAO.find1UserStat("%" + name + "%");
+         
+        title = "Users Report".toUpperCase();
+		keyword = "Filter by" + name; 
+		date = "All time";
+        UsersExcelWriter excelExporter = new UsersExcelWriter(title, keyword, date, listUsers);
+         
+        excelExporter.export(response);  
+	}
+	
+	@GetMapping("/export/users/name={name}/start={start}&end={end}")
+	public void exportUsersByName(HttpServletResponse response, @PathVariable("name") String name, @PathVariable("start") String start, @PathVariable("end") String end) throws IOException,ParseException {
+		response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+         
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=UsersReport_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+        
+        Date newStart = new SimpleDateFormat("yyyy-MM-dd").parse(start);
+		Date newEnd = new SimpleDateFormat("yyyy-MM-dd").parse(end);
+        List<UsersStatisticDto> listUsers = userDAO.find1UserStatByTime("%" + name + "%", newStart, newEnd);
+         
+        title = "Users Report".toUpperCase();
+		keyword = "Filter by " + name; 
+		date = "From " + start + " to " + end;
+        UsersExcelWriter excelExporter = new UsersExcelWriter(title, keyword, date, listUsers);
          
         excelExporter.export(response);  
 	}
 	
 	@GetMapping("/export/productsByMale")
-	public void exportProductsByMale(HttpServletResponse response) throws IOException {
+	public void exportUsersByMale(HttpServletResponse response) throws IOException {
 		response.setContentType("application/octet-stream");
         DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
         String currentDateTime = dateFormatter.format(new Date());
@@ -98,7 +218,28 @@ public class AdminController {
         List<ProductsStatisticDto> listProducts = prodDAO.getBestProductStatisticByMale(1);
         List<ProductImageDto> listProductImage = prodDAO.getProductImage();
          
-        ProductsExcelWriter excelExporter = new ProductsExcelWriter(listProducts, listProductImage);
+        ProductsExcelWriter excelExporter = new ProductsExcelWriter(title, keyword, date, listProducts, listProductImage);
+         
+        excelExporter.export(response);  
+	}
+	
+	@GetMapping("/export/productsByMale/start={start}&end={end}")
+	public void exportProductsByMale(HttpServletResponse response, @PathVariable("start") String start, @PathVariable("end") String end) throws IOException, ParseException {
+		response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+         
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=BestSellerByMale_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+         
+        Date newStart = new SimpleDateFormat("yyyy-MM-dd").parse(start);
+		Date newEnd = new SimpleDateFormat("yyyy-MM-dd").parse(end);
+		int gender = 1;
+        List<ProductsStatisticDto> listProducts = prodDAO.getBestProductStatisticByMaleByTime(gender, newStart, newEnd);
+        List<ProductImageDto> listProductImage = prodDAO.getProductImage();
+         
+        ProductsExcelWriter excelExporter = new ProductsExcelWriter(title, keyword, date, listProducts, listProductImage);
          
         excelExporter.export(response);  
 	}
@@ -116,7 +257,28 @@ public class AdminController {
         List<ProductsStatisticDto> listProducts = prodDAO.getBestProductStatisticByMale(0);
         List<ProductImageDto> listProductImage = prodDAO.getProductImage();
          
-        ProductsExcelWriter excelExporter = new ProductsExcelWriter(listProducts, listProductImage);
+        ProductsExcelWriter excelExporter = new ProductsExcelWriter(title, keyword, date, listProducts, listProductImage);
+         
+        excelExporter.export(response);  
+	}
+	
+	@GetMapping("/export/productsByFemale/start={start}&end={end}")
+	public void exportProductsByFemale(HttpServletResponse response, @PathVariable("start") String start, @PathVariable("end") String end) throws IOException, ParseException {
+		response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+         
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=BestSellerByFemale_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+         
+        Date newStart = new SimpleDateFormat("yyyy-MM-dd").parse(start);
+		Date newEnd = new SimpleDateFormat("yyyy-MM-dd").parse(end);
+		int gender = 0;
+        List<ProductsStatisticDto> listProducts = prodDAO.getBestProductStatisticByMaleByTime(gender, newStart, newEnd);
+        List<ProductImageDto> listProductImage = prodDAO.getProductImage();
+         
+        ProductsExcelWriter excelExporter = new ProductsExcelWriter(title, keyword, date, listProducts, listProductImage);
          
         excelExporter.export(response);  
 	}
--- src/main/java/com/godEShop/Utils/ProductsExcelWriter.java
+++ src/main/java/com/godEShop/Utils/ProductsExcelWriter.java
@@ -1,6 +1,7 @@
 package com.godEShop.Utils;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -12,9 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.util.IOUtils;
 import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
 import org.apache.poi.xssf.usermodel.XSSFDrawing;
@@ -29,21 +32,85 @@ import com.godEShop.Dto.ProductsStatisticDto;
 public class ProductsExcelWriter {
 	private XSSFWorkbook workbook;
     private XSSFSheet sheet;
+    private String title;
+    private String keyword;
+    private String date;
+    private String urlProduct = "src/main/resources/static/upload/ProductImages/";
+    private String logo = "src/main/resources/static/admin/assets/images/Glogo/logo.png";
     private List<ProductsStatisticDto> listProds;
     private List<ProductImageDto> listProdImage;
      
-    public ProductsExcelWriter(List<ProductsStatisticDto> listProds, List<ProductImageDto> listProdImage) {
+    public ProductsExcelWriter(String title, String keyword, String date, List<ProductsStatisticDto> listProds, List<ProductImageDto> listProdImage) {
+    	this.title = title;
+    	this.keyword = keyword;
+    	this.date = date;
         this.listProds = listProds;
         this.listProdImage = listProdImage;
         workbook = new XSSFWorkbook();
     }
     
  
-    private void writeHeaderLine() {
+    private void writeHeaderLine() throws IOException {
         sheet = workbook.createSheet("Report");
         
-        Row row = sheet.createRow(0);
-         
+        int firstRow = 0;
+        int lastRow = 0;
+        int firstCol = 0;
+        int lastCol = 5;
+        		
+        Row title1 = sheet.createRow(0);
+        
+        sheet.addMergedRegion(new CellRangeAddress(firstRow, lastRow, firstCol, lastCol));
+        
+        InputStream my_banner_image = new FileInputStream(logo);
+        byte[] bytes = IOUtils.toByteArray(my_banner_image);
+        int my_picture_id = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
+        my_banner_image.close();
+        XSSFDrawing drawing = (XSSFDrawing) sheet.createDrawingPatriarch();
+        XSSFClientAnchor my_anchor = new XSSFClientAnchor();
+
+        my_anchor.setCol1(0);
+        my_anchor.setRow1(0); 
+        my_anchor.setCol2(2); 
+        my_anchor.setRow2(1);
+        
+        drawing.createPicture(my_anchor, my_picture_id);
+        
+        CellStyle titleStyle = workbook.createCellStyle();
+        XSSFFont fontTitle = workbook.createFont();
+        fontTitle.setBold(true);
+        fontTitle.setColor(IndexedColors.BLUE.getIndex());
+        fontTitle.setFontHeight(30);
+        titleStyle.setFont(fontTitle);
+        titleStyle.setAlignment(HorizontalAlignment.RIGHT);
+        titleStyle.setVerticalAlignment(VerticalAlignment.CENTER);;
+        titleStyle.setWrapText(true);
+        
+        createCell(title1, 0, title + "\n" + keyword, titleStyle);  
+        title1.setHeightInPoints(10*12);
+        
+        Row dateCell = sheet.createRow(1);
+        
+        int firstRowDate = 1;
+        int lastRowDate = 1;
+        int firstColDate = 0;
+        int lastColDate = 5;
+        sheet.addMergedRegion(new CellRangeAddress(firstRowDate, lastRowDate, firstColDate, lastColDate));
+        
+        CellStyle dateStyle = workbook.createCellStyle();
+        XSSFFont fontDate = workbook.createFont();
+        fontDate.setBold(true);
+        fontDate.setItalic(true);
+        fontDate.setColor(IndexedColors.BLUE.getIndex());
+        fontDate.setFontHeight(20);
+        dateStyle.setFont(fontDate);
+        dateStyle.setAlignment(HorizontalAlignment.RIGHT);
+        dateStyle.setVerticalAlignment(VerticalAlignment.CENTER);;
+        dateStyle.setWrapText(true);
+        createCell(dateCell, 0, date, dateStyle); 
+        dateCell.setHeightInPoints(5*7);
+        
+        
         CellStyle style = workbook.createCellStyle();
         XSSFFont font = workbook.createFont();
         font.setBold(true);
@@ -51,17 +118,20 @@ public class ProductsExcelWriter {
         style.setFont(font);
         style.setAlignment(HorizontalAlignment.CENTER);
         style.setWrapText(true);
+        
+        Row headline = sheet.createRow(2);
          
-        createCell(row, 0, "ID", style);      
-        createCell(row, 1, "Image", style);       
-        createCell(row, 2, "Name", style);    
-        createCell(row, 3, "Stock", style);
-        createCell(row, 4, "Sold", style);
+        createCell(headline, 0, "ID", style);      
+        createCell(headline, 1, "Image", style);       
+        createCell(headline, 2, "Name", style);
+        createCell(headline, 3, "Stock", style);
+        createCell(headline, 4, "Sold", style);
     }
      
     private void createCell(Row row, int columnCount, Object value, CellStyle style) {
         sheet.autoSizeColumn(columnCount);
         sheet.setColumnWidth(1, 90*90);
+        sheet.setColumnWidth(2, 120*120);
         Cell cell = row.createCell(columnCount);
         if (value instanceof Integer) {
             cell.setCellValue((Integer) value);
@@ -74,8 +144,7 @@ public class ProductsExcelWriter {
     }
      
     private void writeDataLines() throws IOException {
-    	String urlProduct = "src/main/resources/static/upload/ProductImages/";
-        int rowCount = 1;
+        int rowCount = 3;
         
         CellStyle style = workbook.createCellStyle();
         XSSFFont font = workbook.createFont();
